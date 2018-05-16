a123275954e494dd6cad69c59eabbb199ff6742c
Salome Lambermont <salome.lambermont@gmail.com>
2018-05-16 16:36:24 +0100

    Jquerying

--- src/interface.js
+++ src/interface.js
@@ -12,14 +12,24 @@ $('document').ready(function() {
     $('.temperature').text(thermostat.temperature);
   })
 
+  $('#down').click( function() {
+    thermostat.down();
+    $('.temperature').text(thermostat.temperature);
+  })
+
   function psmClassSetter(){
     return thermostat.isPowerSaving ? 'visible' : 'hidden'
   }
 
-  // $('#PSM').click(function() {
-  //   thermostat.togglePowerSave();
-  //   $('#PSM-status').toggleClass( psmClassSetter()) });
-  //
+
+  $('#PSM').click(function() {
+    thermostat.togglePowerSave();
+    console.log(thermostat.isPowerSaving)
+    $('#PSM-status').toggleClass(
+      psmClassSetter()
+    );
+  });
+
   // for(var i=0; i < 100; i++)  {
   //   $('#message').fadeOut(200).fadeIn(200);
   // }
