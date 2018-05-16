573b71d02f6442dc03204c7fd193011032fa7df6
George Sykes <georgesykes86@gmail.com>
2018-05-16 09:40:04 +0100

    Adds up function

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -7,4 +7,11 @@ describe('Thermostat', function() {
   it('should have a temperature of 20 degrees', function() {
     expect(thermostat.temperature).toEqual(20);
   });
+
+  describe('#Up', function(){
+    it('Increases the temperature by one unit', function(){
+      thermostat.up();
+      expect(thermostat.temperature).toEqual(21);
+    });
+  });
 });
