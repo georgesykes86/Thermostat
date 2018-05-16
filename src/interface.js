56e06fc200d00b335297b82f00998397988cff5c
George Sykes <georgesykes86@gmail.com>
2018-05-16 16:20:13 +0100

    adds up to interface

---
+++ src/interface.js
@@ -0,0 +1,27 @@
+
+
+$('document').ready(function() {
+
+  var thermostat = new Thermostat();
+
+  $('.temperature').text(thermostat.temperature);
+  $('#PSM-status').toggleClass( psmClassSetter() );
+
+  $('#up').click( function() {
+    thermostat.up();
+    $('.temperature').text(thermostat.temperature);
+  })
+
+  function psmClassSetter(){
+    return thermostat.isPowerSaving ? 'visible' : 'hidden'
+  }
+
+  // $('#PSM').click(function() {
+  //   thermostat.togglePowerSave();
+  //   $('#PSM-status').toggleClass( psmClassSetter()) });
+  //
+  // for(var i=0; i < 100; i++)  {
+  //   $('#message').fadeOut(200).fadeIn(200);
+  // }
+
+});
