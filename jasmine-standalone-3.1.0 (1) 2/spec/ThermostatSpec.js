a4105e422bac465d9778da41779ec94c4d02a4ed
Salome Lambermont <salome.lambermont@gmail.com>
2018-05-16 09:31:32 +0100

    first thermostat

---
+++ jasmine-standalone-3.1.0 (1) 2/spec/ThermostatSpec.js
@@ -0,0 +1,10 @@
+describe('Thermostat', function() {
+  var thermostat
+  beforeEach(function() {
+    thermostat = new Thermostat()
+  });
+
+  it('should have a temperature of 20 degrees', function() {
+    expect(thermostat.temperature).toEqual(20);
+  });
+});
